59264f83e97efc3ce9fb94aedccd18cf070dd2c1
Volodymyr Brezitskyi <118307273+VolodX@users.noreply.github.com>
2025-05-26 13:30:18 +0300

    refactor(codebase): update imports and modernize syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
+import AppHeader from './components/appHeader/AppHeader';
+import RandomChar from './components/randomChar/RandomChar';
+import CharList from './components/charList/CharList';
+import CharInfo from './components/charInfo/CharInfo';
 
-function App() {
+import decoration from './resources/img/vision.png';
 
+const App = () => {
   return (
-    <>
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+        <div className="char__content">
+          <CharList />
+          <CharInfo />
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
