fdff33bc6a41357e2b29a93a6d2b09177bf1ef36
Volodymyr Brezitskyi <118307273+VolodX@users.noreply.github.com>
2025-05-28 17:22:09 +0300

    feat(api/marvel): enhance character data handling

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -11,16 +11,37 @@ class MarvelService {
     return res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
-      `${this._apiBase}characters?limit=9&${this._apiKey}`
-    );
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&${this._apiKey}`);
+		// return res.data.results.map(this._transformCharacter);
+		res.data.results.map((item) => {
+			return this._transformCharacter(item);
+		});
   };
 
-  getCharacters = id => {
-    return this.getResource(
-			`${this._apiBase}characters?/${id}${this._apiKey}`
+  getCharacter = async id => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
     );
+
+    if (!res.data || !res.data.results || res.data.results.length === 0) {
+      throw new Error(`Character with ID ${id} not found.`);
+    }
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = char => {
+		if (char.description.length > 100) {
+			char.description = char.description.slice(0, 100) + '...';
+		}
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      id: char.id
+    };
   };
 }
 
