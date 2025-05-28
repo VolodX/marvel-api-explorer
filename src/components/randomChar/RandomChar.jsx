870e1d938d33055a32f4efba93ab6c8c99594513
Volodymyr Brezitskyi <118307273+VolodX@users.noreply.github.com>
2025-05-28 17:23:43 +0300

    refactor(random-char): convert to dynamic data fetching component

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,42 +1,93 @@
-import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
-import mjolnir from '../../resources/img/mjolnir.png';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
 
-const RandomChar = () => {
+import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png';
+import defaultImg from '../../resources/img/default.jpg';
+
+class RandomChar extends Component {
+  state = {
+    char: {}
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.updateChar();
+  }
+
+  onCharLoaded = char => {
+    this.setState({ char });
+  };
+
+  onError = (error) => {
+    console.error(error.message);
+    this.setState({
+      char: {
+        name: 'Unknown Character',
+        description: 'No description provided.',
+        thumbnail: defaultImg,
+        homepage: '#',
+        wiki: '#',
+        id: null
+      }
+    });
+  };
+
+  getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  updateChar = () => {
+    const id = this.getRandomInt(1, 40);
+    this.marvelService
+      .getCharacter(id)
+      .then(this.onCharLoaded)
+      .catch(this.onError);
+  };
+
+  render() {
+    const { name, description, thumbnail, homepage, wiki } = this.state.char;
     return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
-                    </div>
-                </div>
+      <div className="randomchar">
+        <div className="randomchar__block">
+          <img
+            src={thumbnail}
+            alt="Random character"
+            className="randomchar__img"
+          />
+          <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">{description}</p>
+            <div className="randomchar__btns">
+              <a href={homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
+          </div>
         </div>
-    )
+        <div className="randomchar__static">
+          <p className="randomchar__title">
+            Random character for today!
+            <br />
+            Do you want to get to know him better?
+          </p>
+          <p className="randomchar__title">Or choose another one</p>
+          <button className="button button__main">
+            <div className="inner" onClick={this.updateChar}>
+              try it
+            </div>
+          </button>
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RandomChar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import MarvelService from './services/MarvelService.jsx'
 
 import './index.scss'
 import './styles/main.scss'
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)))
-// marvelService.getAllCharacters().then(res => console.log(res));
+// import MarvelService from './services/MarvelService.jsx'
+// const marvelService = new MarvelService();
+// marvelService.getAllCharacters().then(res => console.log(res))
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
